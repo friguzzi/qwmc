5c85cce5c7f7f1c3acc141cc2615d858a051c3c4
Mariia Mykhailova <mamykhai@microsoft.com>
2021-02-26 20:03:53 -0800

    Clean up QMAP and QMPE code

--- Qsharp/qmap/Driver.cs
+++ Qsharp/qmap/Driver.cs
@@ -10,38 +10,30 @@ namespace Quantum.MAP
         static void Main(string[] args)
         {
 
-            using (var qsim = new QuantumSimulator())
+            using var qsim = new QuantumSimulator();
+            int n = 1000;
+            System.Diagnostics.Stopwatch stopwatch = System.Diagnostics.Stopwatch.StartNew();
+            string[] results = new string[n];
+            for (int i = 0; i < n; i++)
             {
-                int n = 1000;
-                System.Diagnostics.Stopwatch stopwatch = System.Diagnostics.Stopwatch.StartNew();
-                string[] results = new string[n];
-                for (int i = 0; i < n; i++)
-                {
-                    IQArray<Result> res = QMAP.Run(qsim).Result;
-                    results[i] = res.ToString();
-                }
-                stopwatch.Stop();
-                Console.WriteLine("Elapsed time: " + stopwatch.ElapsedMilliseconds / 1000.0);
-                PrintResults(results);
+                IQArray<Result> res = QMAP.Run(qsim).Result;
+                results[i] = res.ToString();
             }
+            stopwatch.Stop();
+            Console.WriteLine($"Elapsed time: {stopwatch.ElapsedMilliseconds / 1000.0} sec");
+            PrintResults(results);
         }
         static void PrintResults(string[] results)
         {
             IDictionary<string, int> dict = new Dictionary<string, int>();
-            int result;
             for (int i = 0; i < results.Length; i++)
             {
                 string s = results[i];
-                if (dict.TryGetValue(s, out result))
-                {
-                    dict.Remove(s);
-                    dict.Add(s, result + 1);
-                }
+                if (dict.ContainsKey(s))
+                    dict[s]++;
                 else
                     dict.Add(s, 1);
             }
-
-
             foreach (KeyValuePair<string, int> item in dict)
             {
                 string[] split = item.Key.Split(new Char[] { '[', ',', ']' });
@@ -49,11 +41,10 @@ namespace Quantum.MAP
 
                 for (int i = 1; i < split.Length - 1; i++)
                 {
-                    newkey = newkey + (split[i].Equals("One") ? "1" : "0");
+                    newkey += (split[i].Equals("One") ? "1" : "0");
                 }
                 Console.WriteLine("'{0}': {1},", newkey, item.Value);
             }
-
         }
     }
 }
